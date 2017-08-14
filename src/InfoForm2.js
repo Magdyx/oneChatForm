29ebb88f10bc75729a1307df20c37656591689c7
Abanob Maher <abanobmaher1994@gmail.com>
2017-08-14 11:41:10 +0200

    location input size

--- src/InfoForm2.js
+++ src/InfoForm2.js
@@ -6,7 +6,7 @@ export default class InfoForm2 extends Component {
     const branch = {
       name: 'Zara',
       password: 'password',
-      location: '123, Shawky St., Mostafa Kamel',
+      location: '123, Shawky St., Mostafa Kamel - sidi gabr , Alexandria , Egypt ',
       position: {
           lng: 29.91,
           lat: 31.21
